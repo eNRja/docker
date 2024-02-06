d43af15e25b10ea4127b4c1c43c772252ec3157f
Алексей <satukov90@yandex.ru>
2024-02-06 14:26:08 +0300

    start

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+
+import { StrictMode } from "react";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <div>WORK !!!!!!!!</div>
+  </StrictMode>
+);
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// export default {
+//   define: {
+//     "process.env.MapboxAccessToken": JSON.stringify(
+//       process.env.MapboxAccessToken
+//     ),
+//   },
+// };
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    open: true,
+  },
+  css: {
+    modules: true,
+  },
+});
